bb8b85fe3c3013e403eabe37da41de18c0425824
Noufal <noufal@n.com>
2024-04-27 12:06:08 +0530

    Update the theme

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:calculator/calculator_screen.dart';
 import 'package:flutter/material.dart';
+import 'calculator_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: title,
-      theme: ThemeData.dark(),
+      theme: ThemeData(
+        primaryColor: Colors.indigo, // Change the primary color to indigo
+        hintColor:
+            Colors.orangeAccent, // Change the accent color to orangeAccent
+        scaffoldBackgroundColor:
+            Colors.grey[200], // Set background color to light grey
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            color: Colors.black, // Set headline text color to black
+            fontWeight: FontWeight.bold, // Set headline text weight to bold
+            fontSize: 24, // Set headline font size to 24
+          ),
+        ),
+      ),
       home: const CalculatorScreen(),
     );
   }
